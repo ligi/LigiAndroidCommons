dc4df6bfff264155a9fd4f6ce7c1f31d57e1cce5
marcus bueschleb <ligi@ligi.de>
2011-04-18 06:49:56 +0200

    adding GPLv3 info on top of files

--- src/org/ligi/android/common/bitmap/BitmapScaler.java
+++ src/org/ligi/android/common/bitmap/BitmapScaler.java
@@ -1,3 +1,18 @@
+/*                                                                                                                          
+ * This software is free software; you can redistribute it and/or modify                                                     
+ * it under the terms of the GNU General Public License as published by                                                     
+ * the Free Software Foundation; either version 3 of the License, or                                                        
+ * (at your option) any later version.                                                                                      
+ *                                                                                                                          
+ * This program is distributed in the hope that it will be useful, but                                                      
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY                                               
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License                                                  
+ * for more details.                                                                                                        
+ *                                                                                                                          
+ * You should have received a copy of the GNU General Public License along                                                  
+ * with this program; if not, write to the Free Software Foundation, Inc.,                                                  
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA                                                                    
+ */
 package org.ligi.android.common.bitmap;
 
 import android.graphics.Bitmap;
@@ -7,7 +22,6 @@ import android.view.View;
 /**
  * class to scale bitmaps relative to other stuff
  *  
- * 
  * @author ligi ( aka: Marcus Bueschleb | mail: ligi at ligi dot de )
  *
  * License: GPLv3
@@ -15,7 +29,7 @@ import android.view.View;
 
 public class BitmapScaler {
 
-	public static  Bitmap relative2View(View view,Bitmap orig, float x_scale_, float y_scale_) {
+	public static Bitmap relative2View(View view,Bitmap orig, float x_scale_, float y_scale_) {
 		// create a matrix for the manipulation
 		Matrix matrix = new Matrix();
 		
