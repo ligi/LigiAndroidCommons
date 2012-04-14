51f7f96aa0827ed57dd1cfb465e05b24af5baa11
ligi <ligi@ligi.de>
2012-04-14 22:41:42 +0200

    a better way to lock the orientation - otherwise problems with 3.1

--- src/org/ligi/android/common/activitys/ActivityOrientationLocker.java
+++ src/org/ligi/android/common/activitys/ActivityOrientationLocker.java
@@ -3,9 +3,6 @@ package org.ligi.android.common.activitys;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
-import android.os.Build;
-import android.view.Surface;
-
 
 /**
  * 
@@ -15,42 +12,36 @@ import android.view.Surface;
  */
 public class ActivityOrientationLocker {
 	
-	public static void disableRotation(Activity activity)	{       
-	    final int orientation = activity.getResources().getConfiguration().orientation;
-	    final int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+	public static void disableRotation(Activity activity)	{ 
+		
+		switch (activity.getResources().getConfiguration().orientation){
+        case Configuration.ORIENTATION_PORTRAIT:
+            if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.FROYO){
+            	activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            } else {
+                int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+            if(rotation == android.view.Surface.ROTATION_90|| rotation == android.view.Surface.ROTATION_180){
+            		activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                } else {
+                	activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+            }   
+            break;
 
-	    // fallback values
-	    int SCREEN_ORIENTATION_REVERSE_LANDSCAPE = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-	    int SCREEN_ORIENTATION_REVERSE_PORTRAIT = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-
-	    // after froyo we have real reverse orientations - so use them 
-	    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.FROYO) {
-	        SCREEN_ORIENTATION_REVERSE_LANDSCAPE = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-	        SCREEN_ORIENTATION_REVERSE_PORTRAIT = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-	    } 
-
-	    if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_90)
-	    {
-	        if (orientation == Configuration.ORIENTATION_PORTRAIT)
-	        {
-	            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-	        }
-	        else if (orientation == Configuration.ORIENTATION_LANDSCAPE)
-	        {
-	            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-	        }
-	    }
-	    else if (rotation == Surface.ROTATION_180 || rotation == Surface.ROTATION_270) 
-	    {
-	        if (orientation == Configuration.ORIENTATION_PORTRAIT) 
-	        {
-	            activity.setRequestedOrientation(SCREEN_ORIENTATION_REVERSE_PORTRAIT);
-	        }
-	        else if (orientation == Configuration.ORIENTATION_LANDSCAPE) 
-	        {
-	            activity.setRequestedOrientation(SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
-	        }
-	    }
+        case Configuration.ORIENTATION_LANDSCAPE:
+            if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.FROYO){
+            	activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else {
+                int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+                if(rotation == android.view.Surface.ROTATION_0 || rotation == android.view.Surface.ROTATION_90){
+                	activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                } else {
+                	activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                }
+            }
+            break;
+		}//switch
+	
 	}
 
 	public static void enableRotation(Activity activity) {
